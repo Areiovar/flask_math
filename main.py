b01821bec9a85d7314e2c2364c1d530bf98378c1
Ilya Solohin <118296454+Areiovar@users.noreply.github.com>
2024-04-17 20:09:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
 
 app = Flask(__name__)
+
+def create_table():
+    conn = sqlite3.connect('userdata.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS users
+                 (id INTEGER PRIMARY KEY, surname TEXT, name TEXT, patronymic TEXT,
+                 birthdate TEXT, education TEXT, sex TEXT, about TEXT)''')
+    conn.commit()
+    conn.close()
+
+def insert_data(surname, name, patronymic, birthdate, education, sex, about):
+    conn = sqlite3.connect('userdata.db')
+    c = conn.cursor()
+    c.execute('''INSERT INTO users (surname, name, patronymic, birthdate, education, sex, about)
+                 VALUES (?, ?, ?, ?, ?, ?, ?)''', (surname, name, patronymic, birthdate, education, sex, about))
+    conn.commit()
+    conn.close()
 
 @app.route('/')
 def index():
@@ -26,9 +44,13 @@ def main():
     print("Sex:", sex)
     print("About:", about)
 
-    # Перенаправление на страницу профиля с передачей данных
+    # Вставка данных в базу данных
+    insert_data(surname, name, patronymic, birthdate, education, sex, about)
+
+    # Перенаправление на страницу профиля
     return redirect(url_for('profile', surname=surname, name=name, patronymic=patronymic, birthdate=birthdate,
                             education=education, sex=sex, about=about))
+
 
 @app.route('/account')
 def profile():
@@ -40,10 +62,19 @@ def profile():
     education = request.args.get('education')
     sex = request.args.get('sex')
     about = request.args.get('about')
+    conn = sqlite3.connect ('userdata.db')
+    c = conn.cursor ()
+    c.execute ('SELECT * FROM users ORDER BY id DESC LIMIT 1')
+    user_data = c.fetchone ()
+    conn.close ()
 
     # Отображение шаблона с передачей данных
     return render_template('profile.html', surname=surname, name=name, patronymic=patronymic,
                            birthdate=birthdate, education=education, sex=sex, about=about)
+
+@app.route('/course')
+def course():
+    return 'asd'
 
 
 if __name__ == '__main__':
